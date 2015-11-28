7d3555d6c5acabdd49e56cf1dc7866543e717366
jjsebasv <sebastian.vera@colegiochampagnat.com.ar>
2015-11-28 16:59:29 -0300

    getEmployee

--- aux.c
+++ aux.c
@@ -5,15 +5,15 @@
 int main ()
 {
 struct Employee e1 = { "Pedro", "Gimenez", "Junior", 53853, 1, 1500 };
-struct Employee e2 = { "Pedro", "Gimenez", "Junior", 53853, 1, 1500 };
-struct Employee e3 = { "Pedro", "Gimenez", "Junior", 53853, 1, 1500 };
-struct Employee e4 = { "Pedro", "Gimenez", "Junior", 53853, 1, 1500 };
-struct Employee e5 = { "Pedro", "Gimenez", "Junior", 53853, 1, 1500 };
+struct Employee e2 = { "Pedro", "Gimenez", "Junior", 53854, 1, 1500 };
+struct Employee e3 = { "Pedro", "Gimenez", "Junior", 53855, 1, 1500 };
+struct Employee e4 = { "Pedro", "Gimenez", "Junior", 53856, 1, 1500 };
+struct Employee e5 = { "Pedro", "Gimenez", "Junior", 53857, 1, 1500 };
 struct Employee employees[5] = {e1 , e2 , e3 , e4 , e5};
 int i = 0;
 while ( i < 5 )
 {
-printf( "%s", employees[i].name );
+printf( "%s\n", employees[i].name );
 i = i + 1;
 
 }
--- specialFn.h
+++ specialFn.h
@@ -1,17 +1,41 @@
+#include <ctype.h>
+#include <string.h>
 
 float getSalary(struct Employee e, int lapse, char* timelapse) {
 	int t = lapse;
-	if (strcmp(timelapse, "weeks") == 0) {
+	int salary = e.salary;
+	char aux[7];
+	if (strcmp(timelapse, "WEEK") == 0) {
+		strcpy(aux, "semanas");
 		t /= 4;
 		t *= lapse;
-	} else if (strcmp(timelapse, "years") == 0) {
+	} else if (strcmp(timelapse, "YEAR") == 0) {
+		strcpy(aux, "años");
 		t *= 12;
 		t *= lapse;
-	} else if (strcmp(timelapse, "month") == 0) {
+	} else if (strcmp(timelapse, "MONTH") == 0) {
+		strcpy(aux, "meses");
 		t *= lapse;
 	} else {
 		printf("ERROR\n");
 		return -1;
 	}
+	salary *= t;
+	printf("El empleado cobrara $%d de aca a %d %s \n", salary, lapse, aux  );
 	return t;
+}
+
+struct Employee getEmployee( struct Employee company[], int id) {
+	int i = 0;
+	int size = sizeof(company)/sizeof(company[0]);
+	struct Employee e;
+	while ( i < size ){
+		e = company[i];
+  	if ( e.id == id ){
+  		printEmployee(e);
+  		return e;
+  	}
+  	i++;
+	}
+	return e;
 }
